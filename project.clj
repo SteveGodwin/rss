1c17d7cb33d638836e86a683a6e3c7ae24c77360
Stephen Godwin <stephen.godwin@gmail.com>
2012-04-09 18:41:31 +0100

    first commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject rss "1.0.0-SNAPSHOT"
+  :description "Stephen's first dabblings with Clojure"
+  :main rss
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ src/rss.clj
@@ -0,0 +1,34 @@
+(ns rss
+  (:gen-class)
+  (:require [clojure.xml :as xml])
+  (:require [clojure.set :as set])
+  (:require [clojure.string :as str]))
+
+(defn tag= [tagname]
+  (fn [entry]
+    (= (:tag entry) tagname)))
+
+(defn tagged [tagname trees]
+  (mapcat
+   (fn [tree]
+     (filter (tag= tagname) (:content tree)))
+   trees))
+
+(defn keys-from-rss [location]
+  (->> (list (xml/parse location))
+       (tagged :channel)
+       (tagged :item)
+       (tagged :key)
+       (mapcat :content)))
+
+(defn added [old new]
+  (set/difference new old))
+
+(defn removed [old new]
+  (set/difference old new))
+
+(defn -main [& args]
+  (def old (set (keys-from-rss (first args))))
+  (def new (set (keys-from-rss (fnext args))))
+  (println "Added: " (str/join (added old new)))
+  (println "Removed: " (str/join (removed old new))))
