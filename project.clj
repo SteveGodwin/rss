c9e0b7bd8745fffb8f0dee52addb723c3210addf
Stephen Godwin <stephen.godwin@gmail.com>
2012-04-16 19:54:21 +0100

    Changed to use zip-filter

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject rss "1.0.0-SNAPSHOT"
   :description "Stephen's first dabblings with Clojure"
   :main rss
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.zip "0.1.1"]])
--- src/rss.clj
+++ src/rss.clj
@@ -2,24 +2,12 @@
   (:gen-class)
   (:require [clojure.xml :as xml])
   (:require [clojure.set :as set])
-  (:require [clojure.string :as str]))
+  (:require [clojure.zip :as zip])
+  (:require [clojure.string :as str])
+  (:use [clojure.data.zip.xml]))
 
-(defn tag= [tagname]
-  (fn [entry]
-    (= (:tag entry) tagname)))
-
-(defn tagged [tagname trees]
-  (mapcat
-   (fn [tree]
-     (filter (tag= tagname) (:content tree)))
-   trees))
-
-(defn keys-from-rss [location]
-  (->> (list (xml/parse location))
-       (tagged :channel)
-       (tagged :item)
-       (tagged :key)
-       (mapcat :content)))
+(defn parse-xml [location]
+  (zip/xml-zip (xml/parse location)))
 
 (defn added [old new]
   (set/difference new old))
@@ -27,8 +15,34 @@
 (defn removed [old new]
   (set/difference old new))
 
+(defn item-key [item]
+  (xml1-> item :key text))
+
+(defn insane? [item]
+  (let [time    (xml1-> item :timespent (attr :seconds))
+        points  (xml1-> item :customfields :customfield
+                        [:customfieldname "Story Points"]
+                        :customfieldvalues text)
+        kids (count (xml-> item :subtasks :subtask))]
+    (and (or (nil? points)
+             (= (int (Float/valueOf points)) 0))
+         (or (nil? time)
+             (= (int (Float/valueOf time)) 0))
+         (= kids 0))))
+
+(defn sanity-check [items]
+  (let [insane-items (map item-key (filter insane? items))]
+    (cond (not (empty? insane-items))
+          (println "Items with no time, points or children: "
+                   (str/join " " insane-items)))))
+            
 (defn -main [& args]
-  (def old (set (keys-from-rss (first args))))
-  (def new (set (keys-from-rss (fnext args))))
-  (println "Added: " (str/join " " (added old new)))
-  (println "Removed: " (str/join " " (removed old new))))
+  (def old-items (xml-> (parse-xml (first args)) :channel :item))
+  (def new-items (xml-> (parse-xml (fnext args)) :channel :item))
+
+  (def old-set (set (map item-key old-items)))
+  (def new-set (set (map item-key new-items)))
+
+  (println "Added: " (str/join " " (added old-set new-set)))
+  (println "Removed: " (str/join " " (removed old-set new-set)))
+  (sanity-check new-items))
